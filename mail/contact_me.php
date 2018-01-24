bb90d06e53d8e66cdd834c5c7cb434630b8ca886
Ervin Pepic <ervin.hack@gmail.com>
2018-01-24 15:42:04 +0100

    Design bug fixses and php sendmail form

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -6,7 +6,7 @@ if(empty($_POST['name'])  		||
    empty($_POST['message'])	||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
-	echo "No arguments Provided!";
+	echo "Nema unijetih stavki!";
 	return false;
    }
 	
@@ -16,10 +16,10 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 	
 // Create the email and send the message
-$to = 'hassped@hotmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'hassped@hotmail.com'; // Add your email address inbetween the '' replacing. This is where the form will send a message to.
 $email_subject = "HAS Šped:  $name";
 $email_body = "Dobili ste novu poruku...\n\n"."Ovdje su detalji pošaljiaoca:\n\nIme: $name\n\nEmail: $email_address\n\nTelefon: $phone\n\nPoruka:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: samirmuric78@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Odgovori na: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
