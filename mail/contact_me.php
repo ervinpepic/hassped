b9382da66157c78ef09975bbc1c833641ba307eb
Ervin Pepic <ervin.hack@gmail.com>
2018-01-30 16:25:03 +0100

    Changed email host

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -17,9 +17,9 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 	
 // Create the email and send the message
 $to = 'hassped@hotmail.com'; // Add your email address inbetween the '' replacing. This is where the form will send a message to.
-$email_subject = "HAS Šped:  $name";
-$email_body = "Dobili ste novu poruku...\n\n"."Ovdje su detalji pošaljiaoca:\n\nIme: $name\n\nEmail: $email_address\n\nTelefon: $phone\n\nPoruka:\n$message";
-$headers = "From: hasspedicija@hasspedicija.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$email_subject = "HAS Spedicija:  $name";
+$email_body = "Dobili ste novu poruku...\n\n"."Ovdje su detalji posiljaoca:\n\nIme: $name\n\nEmail: $email_address\n\nTelefon: $phone\n\nPoruka:\n$message";
+$headers = "From: hassped@hassped.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Odgovori na: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
